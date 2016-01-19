fa6b9a5ff1021d6e2b6b5e3811218dd60dda56eb
Marhek <vdheidenMark@gmail.com>
2016-01-19 11:57:12 +0100

    Class Game added

---
+++ Frequency/Game.py
@@ -0,0 +1,11 @@
+﻿class Game(object):
+    """description of class"""
+
+    def __init__(self, state):
+        self.State = state
+
+    def Update(self):
+        return Game(self.State.Update())
+
+    def Draw(self):
+        self.State.Draw()
