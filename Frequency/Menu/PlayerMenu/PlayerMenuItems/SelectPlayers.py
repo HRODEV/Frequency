ea3d7105c38bd2fe4f070ab6e3562b6423698526
Jason Sjambar <realjase@gmail.com>
2016-01-22 02:13:24 -0800

    Start menu with player selection has been completed.
    This lacks the functionality of knowing how many players are selected.

---
+++ Frequency/Menu/PlayerMenu/PlayerMenuItems/SelectPlayers.py
@@ -0,0 +1,21 @@
+﻿'''
+import pygame
+from pygame.surface import Surface
+
+from Menu.StartMenu.StartMenuItems.StartMenuItem import StartMenuItem
+import Vector2
+
+class SelectPlayers:
+
+    def __init__(self, offset: Vector2, image: Surface=pygame.image, rect=None, newState = None):
+        self.NewState = newState
+
+    def Update(self, game):
+        return StartMenuItem.Update(self, game)
+
+    def Draw(self, game):
+        StartMenuItem.Draw(self, game)
+
+    def GetNewState(self):
+        return None # will be the new state
+    '''
