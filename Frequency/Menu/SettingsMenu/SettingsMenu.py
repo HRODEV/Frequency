2d40abf4d85952c6c1a3720c7560414512164236
barld <bakeds@gmail.com>
2016-01-26 09:45:28 +0100

    Change positions

--- Frequency/Menu/SettingsMenu/SettingsMenu.py
+++ Frequency/Menu/SettingsMenu/SettingsMenu.py
@@ -1,4 +1,7 @@
+import pygame
+
 from FormControlls.DropDown import DropDown
+from Helpers import Colors
 from Menu.HeadMenu import HeadMenu
 from Vector2 import Vector2
 
@@ -8,7 +11,7 @@ class SettingsMenu(HeadMenu):
     def __init__(self, resolution, background=None, logo=None, dropDown=None):
         super().__init__(resolution, background, logo)
 
-        self.ResolutionDropDown = dropDown if dropDown is not None else DropDown([Vector2(640,800),Vector2(1920,1080)], Vector2(400,200))
+        self.ResolutionDropDown = dropDown if dropDown is not None else DropDown([Vector2(640,800),Vector2(1920,1080)], Vector2(500,400))
 
     def Update(self, game):
         newDropDown = self.ResolutionDropDown.Update(game)
@@ -16,6 +19,15 @@ class SettingsMenu(HeadMenu):
 
     def Draw(self, game):
         super().Draw(game)
+
+        # Create a font
+        font = pygame.font.Font(None, 25)
+        # Render the text
+        text = font.render("Resolution:", True, Colors.RED)
+        # Create a rectangle
+        textRect = text.get_rect()
+        # Blit the text
+        game.Settings.GetScreen().blit(text, [400, 405])
 
         self.ResolutionDropDown.Draw(game)
 
