65456acdecd115364eaf0af107cc66d62d087675
Andy Bhadai <abhadai@gmail.com>
2016-01-27 10:13:45 +0100

    Menu items are displayed

--- Frequency/Board/MenuRight/MenuRight.py
+++ Frequency/Board/MenuRight/MenuRight.py
@@ -1,65 +1,71 @@
 import pygame
 import Game
+from Helpers import Colors
 from Vector2 import Vector2
 from Helpers.popup import *
 from Helpers.EventHelpers import *
-from GameLogic.Player import import *
+from GameLogic.Player import *
 
-class playerInfoLabels:
-    def __init__(self):
-        pass
-
-class PlayerMoney(playerInfoLabels):
-    def __init__(self, player):
+class PlayerInfoLabel:
+    def __init__(self, player:Player = None):
         self.Player = player
 
     def GetValue(self):
-        return 'Money: ' + str(self.Player.Money)
+        return ""
 
-class PlayerLands(playerInfoLabels):
-    def __init__(self, player):
-        self.Player = player
+    def Draw(self, game: Game, positionInRow):
+        font = pygame.font.SysFont("Arial", 18)
+        position = Vector2((game.Settings.Resolution.X - game.Settings.GetMapSize().X) // 2 + game.Settings.GetMapSize().X, 50)
+        size = Vector2((game.Settings.Resolution.X - game.Settings.GetMapSize().X) // 2, game.Settings.Resolution.Y)
+        game.Settings.GetScreen().blit(font.render(self.GetValue(), True, Colors.WHITE), (position.X + 30, position.Y + (positionInRow * 50)))
 
-    def getValue(self):
+class PlayerMoney(PlayerInfoLabel):
+
+    def GetValue(self):
+        return 'Money: ' + str(0)
+
+class PlayerLands(PlayerInfoLabel):
+
+    def GetValue(self):
         return 'Units: ' # TODO: Needs logic
 
-class PlayerTurnIncome(playerInfoLabels):
-    def __init__(self, player):
-        self.Player = player
+class PlayerTurnIncome(PlayerInfoLabel):
 
-    def getValue(self):
+    def GetValue(self):
         return 'Income this turn: ' + str(0) # TODO: Needs logic
 
-class PlayerNextTurnIncome(playerInfoLabels):
-    def __init__(self, player):
-        self.Player = player
+class PlayerNextTurnIncome(PlayerInfoLabel):
 
-    def getValue(self):
-        return 'Income this turn: ' + str(0) # TODO: Needs logic
+    def GetValue(self):
+        return 'Income next turn: ' + str(0) # TODO: Needs logic
 
-class PlayerRemainingMoves(playerInfoLabels):
-    def __init__(self, player):
-        self.Player = player
+class PlayerRemainingMoves(PlayerInfoLabel):
 
-    def getValue(self):
+    def GetValue(self):
         return 'Remaining moves: ' + str(0) # TODO: Needs logic
 
 
 class MenuRight:
 
-    def __init__(self, game, size=None, position=None):
+    def __init__(self, game, size=None, position=None, playerInfoLabels = None):
         #self.Options = ["Buy units", "Your money", "Lands", "Turn income", "Next turn income", "Remaining moves"]
         self.Size = Vector2((game.Settings.Resolution.X - game.Settings.GetMapSize().X) // 2, game.Settings.Resolution.Y)
         self.Position = Vector2((game.Settings.Resolution.X - game.Settings.GetMapSize().X) // 2 + game.Settings.GetMapSize().X, 0)
         game.Settings.SetMenuLeftSize(self.Size)
         self.Screen = game.Settings.GetScreen()
 
+        self.PlayerInfoLabels = playerInfoLabels if playerInfoLabels is not None \
+            else [PlayerMoney(), PlayerLands(), PlayerNextTurnIncome(), PlayerNextTurnIncome(), PlayerRemainingMoves()]
+
+
+
     def Update(self, game: Game):
         return MenuRight(game, self.Size, self.Position)
 
     def Draw(self, game : Game):
-        self.Font = pygame.font.SysFont("Arial", 18)
-
         pygame.draw.rect(self.Screen,
                          (0, 0, 255),
                          (self.Position.X, self.Position.Y, self.Size.X, self.Size.Y))
+
+        for i in range(0,len(self.PlayerInfoLabels)):
+            self.PlayerInfoLabels[i].Draw(game, i)
