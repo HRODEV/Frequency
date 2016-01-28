a45e2b271216353cbd45d9cee3c727b013af8cc4
barld <bakeds@gmail.com>
2016-01-28 09:29:36 +0100

    delete import

--- Frequency/GameLogic/Unit.py
+++ Frequency/GameLogic/Unit.py
@@ -1,17 +1,17 @@
 from functools import reduce
 
 from GameLogic import Player
-from GameLogic.Map import Tile
+
 
 
 class Unit:
 
-    def __init__(self, tile: Tile, owner: Player):
+    def __init__(self, tile, owner: Player):
         self._tile = tile
         self._owner = owner
 
     @property
-    def Tile(self) -> Tile: return self._tile
+    def Tile(self): return self._tile
     @property
     def Owner(self) -> Player: return self._owner
     @property
@@ -75,3 +75,36 @@ class UnitGroup(Unit):
     @property
     def DefencePoints(self):
         return reduce((lambda aPoints, unit: aPoints + unit.DefencePoints), self._units)
+
+
+class Boat:
+    def __init__(self, tile, owner: Player):
+        self._tile = tile
+        self._owner = owner
+        self._units = []
+
+    @property
+    def Tile(self): return self._tile
+    @property
+    def Owner(self) -> Player: return self._owner
+    @property
+    def DefencePoints(self):
+        return 6
+
+    def AddUnit(self, unit: Unit):
+        if type(unit) is UnitGroup:
+            raise Exception("A UnitGroup can only hold normal units, not UnitGroups")
+        elif len(self._units) > 2:
+            raise Exception("A unitGroup can not hold more than 4 units")
+        else:
+            self._units.append(unit)
+
+    def RemoveUnit(self, unit: Unit):
+        self._units = [_unit for _unit in self._units if _unit != unit]
+
+    @property
+    def Units(self):
+        """
+        :return: it returns a copy
+        """
+        return self._units.copy()
