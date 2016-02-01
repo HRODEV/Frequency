8a99872c390194315c52b5ba4759b7949f2fae8b
Jason Sjambar <realjase@gmail.com>
2016-02-01 11:48:30 +0100

    Shows the name of the player now, requires a string to show the text

--- Frequency/FinalScreen.py
+++ Frequency/FinalScreen.py
@@ -1,4 +1,4 @@
-import pygame
+﻿import pygame
 import Game
 from Menu.HeadMenu import HeadMenu
 from Menu.StartMenu.StartMenuItems.ExitGame import ExitGame
@@ -7,7 +7,7 @@ from Vector2 import Vector2
 
 class FinalScreen(HeadMenu):
 
-    def __init__(self, resolution: Vector2, background=None, logo=None, winner=None):
+    def __init__(self, resolution: Vector2, background=None, logo=None, winner=''):
         super().__init__(resolution, background, logo)
 
         self.ExitButton = ExitGame(Vector2(0, 210))
@@ -23,4 +23,12 @@ class FinalScreen(HeadMenu):
 
     def Draw(self, game: Game):
         super().Draw(game)
+
+        screen_centerX = game.Settings.Resolution.X // 2.4
+        screen_centerY = game.Settings.Resolution.Y // 2
+
+        font = pygame.font.Font(None,30)
+        winnerText = font.render(str(self.Winner)+" is the winner!", 0,(255,255,255))
+        game.Settings.GetScreen().blit(winnerText, (screen_centerX, screen_centerY))
+
         self.ExitButton.Draw(game)
