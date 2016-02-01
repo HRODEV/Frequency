638261571d6b7dbb46cd20dceada06854626fedd
Marhek <vdheidenMark@gmail.com>
2016-02-01 12:36:01 +0100

    Added colors for current player

--- Frequency/Board/MenuLeft/BuyUnitItems.py
+++ Frequency/Board/MenuLeft/BuyUnitItems.py
@@ -7,12 +7,11 @@ from Vector2 import Vector2
 
 
 class BuyUnitItem:
-    def __init__(self, offset: Vector2, playingPlayer, image: Surface=None, hover: Surface=None, rect=None):
+    def __init__(self, offset: Vector2, id, image: Surface=None, hover: Surface=None, rect=None):
         self.Offset = offset
-        self.Image = image if image is not None else self._getTexture()
+        self.Image = image if image is not None else self._getTexture(id)
         self.Hover = hover if hover is not None else self._getHoverTexture()
         self.Rect = rect
-        self.PlayingPlayer = playingPlayer
 
     def Update(self, game):
         return self
@@ -42,32 +41,48 @@ class BuyUnitItem:
 
 class SoldierButton(BuyUnitItem):
 
-    def _getTexture(self):
-        return pygame.transform.scale(pygame.image.load('images/units/soldierGreen.png').convert_alpha(), (38, 38))
-
-    def _getHoverTexture(self):
-        return pygame.transform.scale(pygame.image.load('images/units/soldierRed.png').convert_alpha(), (38, 38))
+    def _getTexture(self, id):
+        if id == 0:
+            return pygame.transform.scale(pygame.image.load('images/units/soldierGreen.png').convert_alpha(), (38, 38))
+        elif id == 1:
+            return pygame.transform.scale(pygame.image.load('images/units/soldierBlue.png').convert_alpha(), (38, 38))
+        elif id == 2:
+            return pygame.transform.scale(pygame.image.load('images/units/soldierYellow.png').convert_alpha(), (38, 38))
+        else:
+            return pygame.transform.scale(pygame.image.load('images/units/soldierRed.png').convert_alpha(), (38, 38))
 
 class RobotButton(BuyUnitItem):
 
-    def _getTexture(self):
-        return pygame.transform.scale(pygame.image.load('images/units/robotGreen.png').convert_alpha(), (38, 38))
-
-    def _getHoverTexture(self):
-        return pygame.transform.scale(pygame.image.load('images/units/robotRed.png').convert_alpha(), (38, 38))
+    def _getTexture(self, id):
+        if id == 0:
+            return pygame.transform.scale(pygame.image.load('images/units/robotGreen.png').convert_alpha(), (38, 38))
+        elif id == 1:
+            return pygame.transform.scale(pygame.image.load('images/units/robotBlue.png').convert_alpha(), (38, 38))
+        elif id == 2:
+            return pygame.transform.scale(pygame.image.load('images/units/robotYellow.png').convert_alpha(), (38, 38))
+        else:
+            return pygame.transform.scale(pygame.image.load('images/units/robotRed.png').convert_alpha(), (38, 38))
 
 class TankButton(BuyUnitItem):
 
-    def _getTexture(self):
-        return pygame.transform.scale(pygame.image.load('images/units/tankGreen.png').convert_alpha(), (38, 38))
-
-    def _getHoverTexture(self):
-        return pygame.transform.scale(pygame.image.load('images/units/tankRed.png').convert_alpha(), (38, 38))
+    def _getTexture(self, id):
+        if id == 0:
+            return pygame.transform.scale(pygame.image.load('images/units/tankGreen.png').convert_alpha(), (38, 38))
+        elif id == 1:
+            return pygame.transform.scale(pygame.image.load('images/units/tankBlue.png').convert_alpha(), (38, 38))
+        elif id == 2:
+            return pygame.transform.scale(pygame.image.load('images/units/tankYellow.png').convert_alpha(), (38, 38))
+        else:
+            return pygame.transform.scale(pygame.image.load('images/units/tankRed.png').convert_alpha(), (38, 38))
 
 class BoatButton(BuyUnitItem):
 
-    def _getTexture(self):
-        return pygame.transform.scale(pygame.image.load('images/units/shipGreen.png').convert_alpha(), (38, 38))
-
-    def _getHoverTexture(self):
-        return pygame.transform.scale(pygame.image.load('images/units/shipRed.png').convert_alpha(), (38, 38))
+    def _getTexture(self, id):
+        if id == 0:
+            return pygame.transform.scale(pygame.image.load('images/units/shipGreen.png').convert_alpha(), (38, 38))
+        elif id == 1:
+            return pygame.transform.scale(pygame.image.load('images/units/shipBlue.png').convert_alpha(), (38, 38))
+        elif id == 2:
+            return pygame.transform.scale(pygame.image.load('images/units/shipYellow.png').convert_alpha(), (38, 38))
+        else:
+            return pygame.transform.scale(pygame.image.load('images/units/shipRed.png').convert_alpha(), (38, 38))
